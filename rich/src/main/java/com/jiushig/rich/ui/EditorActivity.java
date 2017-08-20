0e72ddbce8c39c80ad57806a177ef6eb836a6dd3
guowang <gw@jiushig.com>
2017-08-20 17:57:51 +0800

    基本功能

--- rich/src/main/java/com/jiushig/rich/ui/EditorActivity.java
+++ rich/src/main/java/com/jiushig/rich/ui/EditorActivity.java
@@ -1,9 +1,11 @@
 package com.jiushig.rich.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 
 import com.jiushig.rich.BaseActivity;
 import com.jiushig.rich.R;
@@ -11,6 +13,8 @@ import com.jiushig.rich.R;
 
 public class EditorActivity extends BaseActivity {
 
+    protected EditText title;
+    protected EditText content;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -20,7 +24,10 @@ public class EditorActivity extends BaseActivity {
         initViews();
     }
 
-    private void initViews(){};
+    private void initViews() {
+        title = (EditText) findViewById(R.id.title);
+        content = (EditText) findViewById(R.id.content);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -30,6 +37,35 @@ public class EditorActivity extends BaseActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+        } else if (item.getItemId() == R.id.action_preview) {
+            clickPreview();
+        } else if (item.getItemId() == R.id.action_reference) {
+            clickReference();
+        } else if (item.getItemId() == R.id.action_save) {
+            save(title.getText().toString(), content.getText().toString());
+        }
         return super.onOptionsItemSelected(item);
+    }
+
+    protected void clickPreview() {
+        Intent intent = new Intent(this, PreviewActivity.class);
+        intent.putExtra("text", content.getText().toString());
+        startActivity(intent);
+    }
+
+    protected void clickReference() {
+
+    }
+
+    /**
+     * 保存
+     *
+     * @param title
+     * @param content
+     */
+    protected void save(String title, String content) {
+
     }
 }
--- rich/src/main/java/com/jiushig/rich/ui/PreviewActivity.java
+++ rich/src/main/java/com/jiushig/rich/ui/PreviewActivity.java
@@ -2,8 +2,12 @@ package com.jiushig.rich.ui;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 import com.jiushig.rich.BaseActivity;
+import com.jiushig.rich.R;
+import com.zzhoujay.richtext.RichText;
 
 
 /**
@@ -12,8 +16,28 @@ import com.jiushig.rich.BaseActivity;
 
 public class PreviewActivity extends BaseActivity {
 
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_preview);
+        setToolBar();
+        if (getSupportActionBar() != null)
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        initViews();
+
+    }
+
+    private void initViews() {
+        TextView textView = (TextView) findViewById(R.id.text);
+        RichText.fromMarkdown(getIntent().getStringExtra("text")).into(textView);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
